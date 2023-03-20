cb194012a75320610c78c6dbe07048e3ccc1a39b
Andrew Diego Beers <andrew.d.beers@gmail.com>
2023-03-20 12:27:15 -0400

    Update Nuget Packages. Adjust Error messages to read better. Add new extensions to allow for registration of the enricher

---
+++ TheOmenDen.Shared.Logging/Serilog/EnvironmentLoggerConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Serilog.Configuration;
+
+namespace TheOmenDen.Shared.Logging.Serilog;
+/// <summary>
+/// Adds the <c>WithEventType()</c> extension method to <see cref="LoggerConfiguration"/> 
+/// </summary>
+public static class EnvironmentLoggerConfigurationExtensions
+{
+    /// <summary>
+    /// Adds an <see cref="EventTypeEnricher"/> to the <seealso cref="LoggerConfiguration"/>.
+    /// This allows for various template messages based off of provided <see cref="EventIdIdentifier"/> to be logged out. 
+    /// </summary>
+    /// <param name="enrichmentConfiguration"></param>
+    /// <returns><see cref="LoggerConfiguration"/> for further configuration options to be added</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the provided <paramref name="enrichmentConfiguration"/> is not found</exception>
+    public static LoggerConfiguration WithEventType(this LoggerEnrichmentConfiguration enrichmentConfiguration) =>
+    enrichmentConfiguration is null
+    ? throw new ArgumentNullException(nameof(enrichmentConfiguration))
+    : enrichmentConfiguration.With<EventTypeEnricher>();
+}
--- TheOmenDen.Shared.Logging/Templates/Errors.cs
+++ TheOmenDen.Shared.Logging/Templates/Errors.cs
@@ -9,12 +9,10 @@ public static class Errors
     /// A template for creating an unhandled error dialog
     /// </value>
     public const string UnhandledErrorDebug = @"An unhandled error occurred. {0}";
-
     /// <value>
     /// A template for reporting unhandled errors
     /// </value>
-    public const string UnhandledError = @"An error has occurred in the application" +
-                                         "Please contact our support team if the problem persists, citing the correlation id of the Error Message. Correlation Id: {0}";
+    public const string UnhandledError = @"An error has occurred in the application. Please contact our support team if the problem persists, citing the correlation id of the Error Message. Correlation Id: {0}";
     /// <value>
     /// A template for reporting validation errors
     /// </value>
